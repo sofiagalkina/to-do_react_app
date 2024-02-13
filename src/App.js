e6fe6b77d7916bb25fbb4f289626e2f9e3473673
sofiagalkina <gal18024@byui.edu>
2024-02-12 22:35:17 -0700

    background/font; basic outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import { GoPlusCircle} from "react-icons/go";
+import Todo from './Todo';
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#854f6c] to-[#fbe4d8]`
+}
 
 function App() {
+
+const [todos, setTodos] = useState(['learn how to walk', 'stop drinkoing reign'])
+
   return (
-    <div className="App">
-  
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>To-Do App </h3>
+        <form className={style.form}>
+          <input className={style.input} type='text' placeholder='add a task ..'/>
+          <button className={style.button}><GoPlusCircle size={30} /></button>
+        </form>
+
+        <ul>
+          <Todo />
+        </ul>
+      </div>
     </div>
   );
 }
---
+++ src/Todo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Todo = () => {
+    return (
+        <li>Todooo</li>
+    )
+}
+
+export default Todo
